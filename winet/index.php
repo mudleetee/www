bbe23c8c48089c55ab9fdab2db203b155d2dee8a
mudleetee <namesom78@gmail.com>
2018-08-03 13:22:19 +0700

    610803

---
+++ winet/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<!-- <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″ /> -->
+<meta charset="UTF-8">
+<?php 
+	require '../config.php';
+	// $db->query();
+?>
+
+</body>
+</html>
